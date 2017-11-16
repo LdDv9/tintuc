803b0871cce640c6cbb5f0a3e0dc6fd9526da03d
Dun <ledungb11@gmail.com>
2017-11-16 18:57:19 +0700

    a

--- app/Comment.php
+++ app/Comment.php
@@ -4,13 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
-{
-    protected $table = 'comment';
-    function tintuc(){
-    	return $this->belongsTo('App\TinTuc','idTinTuc','id');
-    }
-    function user(){
-    	return $this->belongsTo('App\User','idUser','id');
-    }
+class Comment extends Model {
+	protected $table = 'comment';
+	function tintuc() {
+		return $this->belongsTo('App\TinTuc', 'idTinTuc', 'id');
+	}
+	function user() {
+		return $this->belongsTo('App\User', 'idUser', 'id');
+	}
 }
+
+// <?php
+
+// namespace App;
+
+// use Illuminate\Database\Eloquent\Model;
+
+// class Comment extends Model
+// {
+//     protected $table = 'comment';
+//     function tintuc(){
+//     	return $this->belongsTo('App\TinTuc','idTinTuc','id');
+//     }
+//     function user(){
+//     	return $this->belongsTo('App\User','idUser','id');
+//     }
+// }
